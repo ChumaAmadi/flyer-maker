f3cfa7a282ce3ac387c4fe3edcfc112536622724
chuma <C.NnodimAmadi001@umb.edu>
2023-02-22 06:57:35 -0500

    First Commit 2/22/2023

--- src/canvas.js
+++ src/canvas.js
@@ -1,88 +1,111 @@
-import React, { useState } from 'react';
-import { fabric } from 'fabric';
-import Navbar from './Navbar';
+import React, { useState } from "react";
+import { fabric } from "fabric";
+import { saveAs } from "file-saver";
+import jsPDF from "jspdf";
 
-function Canvas() {
-  const [canvas, setCanvas] = useState(null);
-  const [text, setText] = useState('');
-
-  const handleImage = (e) => {
-    const file = e.target.files[0];
-    const reader = new FileReader();
-
-    reader.onload = (event) => {
-      const imgObj = new Image();
-      imgObj.src = event.target.result;
-
-      imgObj.onload = () => {
-        const canvas = new fabric.Canvas('canvas');
-        setCanvas(canvas);
-
-        // set canvas dimensions to fit entire page
-        canvas.setDimensions({
-          width: 1000,
-          height: 700
-        });
-
-        const image = new fabric.Image(imgObj);
-
-        // set image dimensions to fit canvas and center it
-        const scaleFactor = Math.min(canvas.width / image.width, canvas.height / image.height);
-        const imgWidth = image.width * scaleFactor;
-        const imgHeight = image.height * scaleFactor;
-        image.set({
-          width: imgWidth,
-          height: imgHeight,
-          left: (canvas.width - imgWidth) / 2,
-          top: (canvas.height - imgHeight) / 2
-        });
-
-        canvas.add(image);
-      };
-    };
-
-    reader.readAsDataURL(file);
-  };
+const Canvas = () => {
+  const [text, setText] = useState("");
+  const [font, setFont] = useState("");
+  const [fontSize, setFontSize] = useState(40);
 
   const handleTextChange = (e) => {
     setText(e.target.value);
   };
 
-  const addText = () => {
-    const textObject = new fabric.Text(text, {
+  const handleFontChange = (e) => {
+    setFont(e.target.value);
+  };
+
+  const handleFontSizeChange = (e) => {
+    setFontSize(e.target.value);
+  };
+
+  const handleClearCanvas = () => {
+    const canvas = new fabric.Canvas("canvas");
+    canvas.clear();
+  };
+
+  const handleDownloadPdf = () => {
+    const canvas = new fabric.Canvas("canvas");
+    const imgData = canvas.toDataURL({
+      format: "jpeg",
+      quality: 0.8,
+    });
+    const pdf = new jsPDF();
+    pdf.addImage(imgData, "JPEG", 0, 0);
+    pdf.save("download.pdf");
+  };
+
+  const handleImage = (e) => {
+    const reader = new FileReader();
+    reader.onload = function (event) {
+      const imgObj = new Image();
+      imgObj.src = event.target.result;
+      imgObj.onload = function () {
+        const canvas = new fabric.Canvas("canvas");
+        const canvasWidth = canvas.width;
+        const canvasHeight = canvas.height;
+        const imgAspectRatio = imgObj.width / imgObj.height;
+        const canvasAspectRatio = canvasWidth / canvasHeight;
+        let scaleFactor = 1;
+
+        if (imgAspectRatio > canvasAspectRatio) {
+          // image is wider than canvas
+          scaleFactor = canvasWidth / imgObj.width;
+        } else {
+          // image is taller than canvas
+          scaleFactor = canvasHeight / imgObj.height;
+        }
+
+        const image = new fabric.Image(imgObj, {
+          left: 0,
+          top: 0,
+          scaleX: scaleFactor,
+          scaleY: scaleFactor,
+        });
+
+        canvas.setBackgroundImage(image, canvas.renderAll.bind(canvas));
+        canvas.setWidth(canvasWidth);
+        canvas.setHeight(canvasHeight);
+        canvas.renderAll();
+      };
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
+
+  const handleAddText = () => {
+    const canvas = new fabric.Canvas("canvas");
+    const textObj = new fabric.Text(text, {
       left: canvas.width / 2,
       top: canvas.height / 2,
-      fill: '#fff'
-      
+      fontFamily: font,
+      fontSize: parseInt(fontSize),
+      fill: "#000000",
     });
-
-    canvas.add(textObject);
+    canvas.add(textObj);
   };
 
   return (
-    <div>
-      <Navbar onImageUpload={handleImage} />
-      <div className="container">
-        <canvas id="canvas" />
-        <div className="field has-addons">
-          <div className="control">
-            <input
-              className="input"
-              type="text"
-              placeholder="Enter Text"
-              value={text}
-              onChange={handleTextChange}
-            />
-          </div>
-          <div className="control">
-            <button className="button is-primary" onClick={addText}>
-              Add Text
-            </button>
-          </div>
-        </div>
-      </div>
+    <div className="canvas-container">
+      <nav>
+        <input type="file" id="imgLoader" onChange={handleImage} />
+        <button onClick={handleAddText}>Add Text</button>
+        <button onClick={handleClearCanvas}>Clear Canvas</button>
+        <button onClick={handleDownloadPdf}>Download PDF</button>
+        <input type="text" placeholder="Enter Text" onChange={handleTextChange} />
+        <input type="text" placeholder="Enter Font" onChange={handleFontChange} />
+        <input
+          type="number"
+          placeholder="Enter Font Size"
+          min="1"
+          max="100"
+          value={fontSize}
+          onChange={handleFontSizeChange}
+        />
+      </nav>
+      <canvas id="canvas" width={950} height={800} className="canvas-style"></canvas>
     </div>
   );
-}
+};
 
 export default Canvas;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ImageLoader from './canvas';
+import Canvas from './canvas';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <ImageLoader />
+    <Canvas />
   </React.StrictMode>
 );
 
